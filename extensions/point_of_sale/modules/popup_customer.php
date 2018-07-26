167c5fca4fa6db3b3f1d6b24ba85b3af50b41f56
justinharings <32236972+justinharings@users.noreply.github.com>
2018-07-26 11:44:46 +0200

    Problematiek in popup input velden

--- extensions/point_of_sale/modules/popup_customer.php
+++ extensions/point_of_sale/modules/popup_customer.php
@@ -44,7 +44,16 @@ if(isset($_GET['key']))
 		<link rel="stylesheet" type="text/css" href="/library/css/pos.css" />
 
 		<script type="text/javascript" src="//code.jquery.com/jquery-latest.js"></script>
-		<script type="text/javascript" src="/library/js/motherboard.minified.js"></script>
+		
+		<script type="text/javascript" src="/library/js/dashboard.minified.js"></script>
+		<script type="text/javascript" src="/library/js/datepicker.minified.js"></script>
+		<script type="text/javascript" src="/library/js/emails.minified.js"></script>
+		<script type="text/javascript" src="/library/js/framework.minified.js"></script>
+		<script type="text/javascript" src="/library/js/input.minified.js"></script>
+		<script type="text/javascript" src="/library/js/multiselect.minified.js"></script>
+		<script type="text/javascript" src="/library/js/notes.minified.js"></script>
+		<script type="text/javascript" src="/library/js/sms.minified.js"></script>
+		
 		<script type="text/javascript" src="/library/js/pos.js"></script>
 	</head>
 
--- extensions/workshop/modules/popup_documentation_view.php
+++ extensions/workshop/modules/popup_documentation_view.php
@@ -36,7 +36,16 @@ $doc = $mb->_runFunction("workorders", "loadDocumentation", array($_GET['key']))
 <html lang="nl">
 	<head>
 		<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
-		<link rel="stylesheet" type="text/css" href="/library/css/motherboard.minified.css" />
+		
+		<script type="text/javascript" src="/library/js/dashboard.minified.js"></script>
+		<script type="text/javascript" src="/library/js/datepicker.minified.js"></script>
+		<script type="text/javascript" src="/library/js/emails.minified.js"></script>
+		<script type="text/javascript" src="/library/js/framework.minified.js"></script>
+		<script type="text/javascript" src="/library/js/input.minified.js"></script>
+		<script type="text/javascript" src="/library/js/multiselect.minified.js"></script>
+		<script type="text/javascript" src="/library/js/notes.minified.js"></script>
+		<script type="text/javascript" src="/library/js/sms.minified.js"></script>
+		
 		<link rel="stylesheet" type="text/css" href="/library/css/pos.css" />
 
 		<script type="text/javascript" src="//code.jquery.com/jquery-latest.js"></script>
